02521c8ce3c68a5fbe6fa6a2ef56d2fe1ff3ef80
Katja Hvid Krogh <122166841+Katjahkrogh@users.noreply.github.com>
2025-02-10 12:22:29 +0100

    initial fetch with axios

--- rawg-client/src/App.tsx
+++ rawg-client/src/App.tsx
@@ -1,5 +1,6 @@
 import { Grid, GridItem, Show } from '@chakra-ui/react';
 import NavBar from './components/NavBar';
+import { GameGrid } from './components/GameGrid';
 
 function App() {
   return (
@@ -8,16 +9,15 @@ function App() {
         base: `"header" "main"`,
         lg: `"header header" "aside main"`,
       }}>
-      <GridItem pl="2" bg="orange.300" area={'header'}>
+      <GridItem pl="2" area={'header'}>
         <NavBar/>
       </GridItem>
       <Show above='lg'>
-        <GridItem pl="2" bg="pink.300" area={'aside'}>
-          Aside
+        <GridItem pl="2" area={'aside'}>
         </GridItem>
       </Show>
-      <GridItem pl="2" bg="green.300" area={'main'}>
-        Main
+      <GridItem pl="2" area={'main'}>
+        <GameGrid/>
       </GridItem>
     </Grid>
   );
