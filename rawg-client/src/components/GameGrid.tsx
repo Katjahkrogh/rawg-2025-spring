2bd44b763d78485a672b6d9583bac5637b0b5765
Katja Hvid Krogh <122166841+Katjahkrogh@users.noreply.github.com>
2025-02-17 09:25:09 +0100

    game card

--- rawg-client/src/components/GameGrid.tsx
+++ rawg-client/src/components/GameGrid.tsx
@@ -1,17 +1,21 @@
-import { Text } from '@chakra-ui/react';
-import { useGames } from '../hooks/useGames';
+import { SimpleGrid, Text } from '@chakra-ui/react';
+import useGames  from '../hooks/useGames';
+import GameCard from './GameCard';
 
-export const GameGrid = () => {
+
+const GameGrid = () => {
   const { games, error } = useGames();
 
   return (
     <div>
       {error && <Text color="tomato">{error}</Text>}
-      <ul>
+      <SimpleGrid columns={{ base: 1, md: 2, lg: 3, xl: 4 }} spacing={4} padding="10">
         {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
+          <GameCard key={game.id} game={game}/>
         ))}
-      </ul>
+      </SimpleGrid>
     </div>
   );
 };
+
+export default GameGrid;
--- rawg-client/src/hooks/useGames.ts
+++ rawg-client/src/hooks/useGames.ts
@@ -1,9 +1,10 @@
-import { useState, useEffect } from "react";
-import apiClient from "../services/api-client";
+import { useState, useEffect } from 'react';
+import apiClient from '../services/api-client';
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 
 interface GameResponse {
@@ -11,16 +12,21 @@ interface GameResponse {
   results: Game[];
 }
 
-export const useGames = () => {
+const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState('');
 
   useEffect(() => {
+    const controller = new AbortController();
     apiClient
-      .get<GameResponse>('/games')
+      .get<GameResponse>('/games', { signal: controller.signal })
       .then((res) => setGames(res.data.results))
       .catch((err) => setError(err.message));
+
+    return () => controller.abort();
   }, []);
 
-  return {games, error};
+  return { games, error };
 };
+
+export default useGames;
