0bd86683f43cfb7117ad7609b15b09afe57857e9
Katja Hvid Krogh <122166841+Katjahkrogh@users.noreply.github.com>
2025-02-24 09:15:20 +0100

    useData hook

--- rawg-client/src/components/GenreList.tsx
+++ rawg-client/src/components/GenreList.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import useGenres from '../hooks/useGenres';
 
 const GenreList = () => {
-  return (
-    <div>GenreList</div>
-  )
-}
+  const { data: genres, error, isLoading } = useGenres();
 
-export default GenreList
+  return (
+    <div>
+      {error && <p>{error}</p>}
+      {isLoading && <p>Loading...</p>}
+      <ul>
+        {genres.map((genres) => (
+          <li key={genres.id}>{genres.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default GenreList;
