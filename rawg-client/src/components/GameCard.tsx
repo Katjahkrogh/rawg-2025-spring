352a9245747ce79c3f7a5c5f77a3f7679d7b8fcf
Katja Hvid Krogh <122166841+Katjahkrogh@users.noreply.github.com>
2025-04-07 10:03:46 +0200

    emoji added

--- rawg-client/src/components/GameCard.tsx
+++ rawg-client/src/components/GameCard.tsx
@@ -1,8 +1,9 @@
-import { Card, CardBody, Heading, HStack, Image} from '@chakra-ui/react';
+import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react';
 import { Game } from '../hooks/useGames';
 import PlatformIconsList from './PlatformIconsList';
 import CriticScore from './CriticScore';
 import getCroppedImageUrl from '../services/image-url';
+import Emoji from './Emoji';
 
 interface Props {
   game: Game;
@@ -15,9 +16,14 @@ const GameCard = ({ game }: Props) => {
       <CardBody>
         <HStack justifyContent="space-between" marginBottom={1.5}>
           <PlatformIconsList platforms={game.parent_platforms.map((p) => p.platform)} />
-          <CriticScore score={game.metacritic}/>
+          <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize="2xl">{game.name}</Heading>
+        <Heading fontSize="2xl">
+          <HStack>
+            <Text>{game.name}</Text>
+            <Emoji rating_top={game.rating_top} />
+          </HStack>
+        </Heading>
       </CardBody>
     </Card>
   );
